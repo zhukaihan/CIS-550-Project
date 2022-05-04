cd4e440628b66497d24a3b4084692b8f82f94591
Peter Zhu <kaihan.zhu@zhukaihan.com>
2022-05-04 11:24:33 -0700

    Prices Table Completed

--- frontend/src/pages/prices/PricePage.js
+++ frontend/src/pages/prices/PricePage.js
@@ -51,28 +51,21 @@ function PricePage() {
           // };
       }} dataSource={prices} pagination={{ pageSizeOptions:[5, 10], defaultPageSize: 5, showQuickJumper:true }}>
           <ColumnGroup title="Time">
-            <Column title="Timestamp" dataIndex="Timestamp" key="Timestamp" sorter= {(a, b) => a.Timestamp.localeCompare(b.Timestamp)}/>
-            {/* <Column title="Away" dataIndex="Away" key="Away" sorter= {(a, b) => a.Away.localeCompare(b.Away)}/> */}
+            <Column title="Date" dataIndex="Date" key="Date" sorter = {(a, b) => a.Date.localeCompare(b.Date)}/>
+            <Column title="Timestamp" dataIndex="Timestamp" key="Timestamp" sorter = {(a, b) => a.Timestamp.localeCompare(b.Timestamp)}/>
           </ColumnGroup>
+          <Column title="Symbol" dataIndex="Symbol" key="Symbol"/>
           <ColumnGroup title="Prices">
             <Column title="Open" dataIndex="Open" key="Open" sorter= {(a, b) => a.Open - b.Open}/>
             <Column title="Close" dataIndex="CLose" key="CLose" sorter= {(a, b) => a.CLose - b.CLose}/>
+            <Column title="High" dataIndex="High" key="High" sorter= {(a, b) => a.High - b.High}/>
+            <Column title="Low" dataIndex="Low" key="Low" sorter= {(a, b) => a.Low - b.Low}/>
           </ColumnGroup>
-          <Column title="Date" dataIndex="Date" key="Date"/>
-          <Column title="Time" dataIndex="Time" key="Time"/>
+          <ColumnGroup title="Volume">
+            <Column title="Volume_Crypto" dataIndex="Volume_Crypto" key="Volume_Crypto" sorter= {(a, b) => a.Volume_Crypto - b.Volume_Crypto}/>
+            <Column title="Volume_Currency" dataIndex="Volume_Currency" key="Volume_Currency" sorter= {(a, b) => a.Volume_Currency - b.Volume_Currency}/>
+          </ColumnGroup>
       </Table>
-      <table>
-        {
-          prices.map((price, index) => {
-            // "Timestamp":1444435200,"Date":"2015-10-10 00:00:00","Symbol":"BTC/US","Open":245.39,"High":245.39,"Low":245.39,"CLose":245.39,"Volume_Crypto":0,"Volume_Currency":0
-            return (
-              <tr>
-                {price.Timestamp}{price.CLose}
-              </tr>
-            ) // TODO, change variable name. 
-          })
-        }
-      </table>
 
       {
         //   <PriceByDateRangeGraph
